b44d60721b5dcddf646f0a9e6649ef9a896d181e
dom767 <dominicpenfold@gmail.com>
2016-01-28 21:44:20 +0000

    Blend Types
    Now supports lerp and binary chop

--- WooFractal/OrbitColours.cs
+++ WooFractal/OrbitColours.cs
@@ -7,8 +7,17 @@ namespace WooFractal
 {
     public class OrbitColours
     {
+        public enum EBlendType
+        {
+            Linear=0,
+            Chop=1,
+        };
+
+        public EBlendType _BlendType = EBlendType.Linear;
         public Material _StartColour = new Material();
         public Material _EndColour = new Material();
         public double _Multiplier = 1.0;
+        public double _Offset = 0.0;
+        public double _Power = 0.0;
     }
 }
